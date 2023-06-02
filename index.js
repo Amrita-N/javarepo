06b93f7ab25b5a4b0545c21746d6628b1fceccd9
am <amrita_na84@yhaoo.co.in>
2023-06-02 16:21:32 +0530

    commit

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 //importing node framework
 var express = require(‘express’);
-
-
-
 var app = express();
-//Respond with "hello world" for requests that hit our root "/"
+//Respond with "hello World" for requests that hit our root "/"
 app.get(‘/’, function (req, res) {
- res.send(‘hello india’);
+ res.send(‘hello India’);
 });
 //listen to port 3000 by default
 app.listen(process.env.PORT || 3000);
