c29d0abcca6144c77c75aa1e506d62088d2c575d
amrita <amrita_na84@yahoo.co.in>
2023-07-01 11:20:56 +0530

    first

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-var express = require('express');
+//importing node framework
+var express = require(‘express’);
+ 
 var app = express();
-app.get('/', function (req, res) {
-res.send('hello world');
+//Respond with "hello world" for requests that hit our root "/"
+app.get(‘/’, function (req, res) {
+ res.send(‘hello world’);
 });
-app.listen(process.env.PORT || 80);
+//listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+ 
 module.exports = app;
